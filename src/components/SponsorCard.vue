82bdd1314220516114b39e44e7c2b943f0d3d180
0xEquinox <WilliamJMcdonald@protonmail.com>
2022-10-03 17:27:08 -0700

    Linked up the this could be you cards with the contact page

--- src/components/SponsorCard.vue
+++ src/components/SponsorCard.vue
@@ -1,6 +1,8 @@
 <template>
   <div
-    class="cursor-pointer transition flex justify-center items-center w-full max-w-[20rem] aspect-square bg-gray-300 hover:bg-gray-200 rounded-2xl">
+    class="cursor-pointer transition flex justify-center items-center w-full max-w-[20rem] aspect-square bg-gray-300 hover:bg-gray-200 rounded-2xl"
+    @click="goToContact"
+    >
     <h1 class="flex text-center text-2xl font-semibold">
       {{ text }}
     </h1>
@@ -8,7 +10,15 @@
 </template>
 
 <script setup>
+  import { useRouter } from "vue-router";
+
   defineProps({
     text: String,
   });
+
+const router = useRouter();
+
+const goToContact = () => {
+  router.push("/contact");
+};
 </script>
