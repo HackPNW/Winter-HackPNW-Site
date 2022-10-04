b50f3863e838cd8c38e2f1c743522384b0af6d0f
sexnine <25860388+sexnine@users.noreply.github.com>
2022-10-04 16:49:40 +1100

    Formatting

--- src/components/SponsorCard.vue
+++ src/components/SponsorCard.vue
@@ -1,8 +1,7 @@
 <template>
   <div
     class="cursor-pointer transition flex justify-center items-center w-full max-w-[20rem] aspect-square bg-gray-300 hover:bg-gray-200 rounded-2xl"
-    @click="goToContact"
-    >
+    @click="goToContact">
     <h1 class="flex text-center text-2xl font-semibold">
       {{ text }}
     </h1>
@@ -16,9 +15,9 @@
     text: String,
   });
 
-const router = useRouter();
+  const router = useRouter();
 
-const goToContact = () => {
-  router.push("/contact");
-};
+  const goToContact = () => {
+    router.push("/contact");
+  };
 </script>
