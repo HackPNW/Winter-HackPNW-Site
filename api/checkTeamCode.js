121843cbe5ce100e14c2813ce0e775c81208fbb9
sexnine <25860388+sexnine@users.noreply.github.com>
2022-10-06 17:47:24 +1100

    woo backend !

--- api/checkTeamCode.js
+++ api/checkTeamCode.js
@@ -9,15 +9,15 @@ export default async function handler(request, response) {
   if (request.method !== "GET")
     return response.status(405).send("Method not allowed");
 
-  await client.connect();
-  const db = client.db(dbName);
-  const collection = db.collection("teams");
-
-  const teamCode = request.body.code;
+  const teamCode = request.query.code;
 
   if (teamCode == null || !/^\d{4}-\d{4}$/.test(teamCode)) {
     return response.status(200).json({ valid: false });
   }
+
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection("teams");
 
   const res = await collection.findOne({ code: teamCode });
   if (res == null) {
--- api/register.js
+++ api/register.js
@@ -1,3 +1,5 @@
+const yup = require("yup");
+const _ = require("lodash");
 const { MongoClient } = require("mongodb");
 
 const url = process.env.MONGO_URL;
@@ -5,22 +7,120 @@ const client = new MongoClient(url);
 
 const dbName = "hackpnw";
 
+const colorOptions = ["red", "amber", "green", "cyan", "violet", "pink"];
+const gradeOptions = ["9", "10", "11", "12"];
+
+const formSchema = yup.object({
+  firstName: yup.string().required().label("First Name"),
+  lastName: yup.string().required().label("Last Name"),
+  email: yup.string().required().email().label("Email"),
+  school: yup.string().required().label("High School"),
+  grade: yup
+    .string()
+    .oneOf(gradeOptions, "Grade cannot be empty")
+    .label("Grade"),
+  color: yup
+    .string()
+    .oneOf(colorOptions, "Favourite Color cannot be empty")
+    .label("Favourite Color"),
+  github: yup.string().matches(/^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i, {
+    message: "GitHub username is not valid",
+    excludeEmptyString: true,
+  }),
+  createTeam: yup.bool().required(),
+  hasTeamCode: yup
+    .bool()
+    .when("createTeam", { is: false, then: (x) => x.required() }),
+  teamCode: yup
+    .string()
+    .label("Team Code")
+    .when(["createTeam", "hasTeamCode"], {
+      is: (createTeam, hasTeamCode) => !createTeam && hasTeamCode,
+      then: (x) =>
+        x.required().matches(/^\d{4}-\d{4}$/, "Team Code is not valid"), // TODO: backend check
+    }),
+  teamName: yup
+    .string()
+    .label("Team Name")
+    .when("createTeam", {
+      is: true,
+      then: (x) =>
+        x
+          .required()
+          .matches(
+            /^[a-z\d ]*$/,
+            "Team name must only contain: letters, numbers and spaces"
+          )
+          .min(5)
+          .max(20),
+    }),
+  fillTeam: yup.bool().when("createTeam", {
+    is: true,
+    then: (x) => x.required(),
+  }),
+});
+
+async function createTeam(teamName, db) {
+  const collection = db.collection("teams");
+
+  let code = Math.floor(Math.random() * 1_0000_0000).toString();
+  code = code.slice(0, 4) + "-" + code.slice(4, 8);
+
+  const doc = {
+    name: teamName,
+    code,
+  };
+
+  await collection.insertOne(doc);
+
+  return doc;
+}
+
 export default async function handler(request, response) {
   if (request.method !== "POST")
     return response.status(405).send("Method not allowed");
 
-  // TODO: Checks
+  // Checks
+  let form = request.body;
+  console.log(form);
+  try {
+    form = await formSchema.validate(form);
+  } catch (e) {
+    console.log(":(");
+    console.log(e);
+    return response.status(400).send(e);
+  }
 
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection("registrations");
 
-  const doc = { ...request.body };
+  const doc = _.pick(form, [
+    "firstName",
+    "lastName",
+    "email",
+    "school",
+    "grade",
+    "color",
+    "github",
+  ]);
+
+  let teamCode = null;
+  if (form.createTeam) {
+    const teamDoc = await createTeam(form.teamName, db);
+    console.log(teamDoc);
+    doc.teamId = teamDoc._id;
+    teamCode = teamDoc.code;
+  } else {
+    doc.teamId = (
+      await db.collection("teams").findOne({ code: form.teamCode })
+    )._id;
+  }
 
   await collection.insertOne(doc);
 
   response.status(200).json({
     success: true,
-    doc,
+    teamCode,
   });
 }
