9bf8ac532ce378c0174dfc23f39fcd8c54dd124f
0xEquinox <WilliamJMcdonald@protonmail.com>
2022-10-27 18:41:45 -0700

    made the contact us page responsive

--- src/views/ContactUsView.vue
+++ src/views/ContactUsView.vue
@@ -21,24 +21,24 @@
       <div class="h-full mx-6">
         <div class="flex flex-col justify-between p-4 md:p-10">
           <div
-            class="2xl:my-32 xl:my-24 md:my-12 flex justify-between items-center relative">
+            class="2xl:my-32 xl:my-24 md:my-12 flex flex-1 flex-col space-y-8 lg:flex-row lg:justify-between items-center relative">
             <div>
               <h1
-                class="lg:text-9xl text-5xl md:8xl sm:text-8xl font-bold font-poppins flex-0 underline underline-offset-8 decoration-red-500">
+                class="mt-3 lg:text-9xl text-5xl md:8xl sm:text-8xl font-bold font-poppins flex-0 underline underline-offset-8 decoration-red-500">
                 Contact <br />
                 Us!
               </h1>
               <hr class="mt-12 h-12 border-t-8 border-black" />
-              <div class="w-[35.08rem]">
+              <div class="lg:w-[35.08rem]">
                 <h1 class="text-gray-700 ld xl:text-3xl">
                   We'd love to answer any of your questions or inqueries
                 </h1>
               </div>
             </div>
             <div
-              class="space-y-2 font-semibold text-2xl text-gray-700 hidden xl:block">
-              <div class="bg-slate-200 p-8 rounded-xl">
-                <h1 class="text-6xl font-bold text-black">Emails:</h1>
+              class="space-y-2 font-semibold md:text-2xl text-lg text-gray-700 xl:block">
+              <div class="bg-slate-200 md:p-8 p-4 rounded-xl">
+                <h1 class="lg:text-6xl md:text-5xl text-3xl font-bold text-black ">Emails:</h1>
                 <h1 class="pt-4">
                   <span class="text-purple-500 font-bold">General</span>:
                   team@hackpnw.org
@@ -49,15 +49,15 @@
                 </h1>
               </div>
 
-              <div class="p-8 rounded-xl bg-slate-200">
+              <div class="md:p-8 p-4 rounded-xl bg-slate-200">
                 <div class="flex flex-col">
-                  <p class="text-lg text-gray-800 font-semibold">Email</p>
+                <p class="lg:text-4xl lg:pb-2 md:text-3xl text-xl text-black font-semibold">Email</p>
                   <input
-                    class="px-2 py-1 text-lg bg-gray-100 rounded-md outline-0 mb-2"
+                    class="px-2 py-1 text-lg bg-gray-100 rounded-md outline-0 mb-2 md:placeholder:text-lg placeholder:text-sm"
                     placeholder="your-email@example.com" />
-                  <p class="text-lg text-gray-800 font-semibold">Message</p>
+                  <p class="lg:text-4xl lg:pb-2 md:pb-1 md:text-3xl text-xl text-black font-semibold">Message</p>
                   <textarea
-                    class="px-2 py-1 text-lg bg-gray-100 rounded-md outline-0 mb-4"
+                  class="px-2 py-1 text-lg bg-gray-100 rounded-md outline-0 mb-4 md:placeholder:text-lg placeholder:text-sm"
                     rows="2"
                     placeholder="Hey there..." />
                   <button
