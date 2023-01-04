a6acd172a5d091669597a4ad0fac4d90465bead1
0xEquinox <WilliamJMcdonald@protonmail.com>
2023-01-03 17:29:08 -0800

    updated faq

--- src/faqs.js
+++ src/faqs.js
@@ -14,7 +14,7 @@ const faqs = [
   ],
   [
     "How many events are there?",
-    "There is a main team challenge along with a side competition for comptetive programming. To break up the hacking, each day there will be one fun event!",
+    "There is a main team challenge, and to break up the hacking, each day there will be one fun event!",
   ],
   ["Will there be prizes?", "Of course! There will be many prizes to win!"],
   [
