4f194ea3240a0c631c2409f599a3f56f9fb7522c
0xEquinox <WilliamJMcdonald@protonmail.com>
2023-01-04 20:53:59 -0800

    FR fixed email, now it will work

--- api/register.js
+++ api/register.js
@@ -126,7 +126,8 @@ export default async function handler(request, response) {
 
   await collection.insertOne(doc);
 
-  if (form.teamName != undefined && form.teamCode != undefined) await sendEmail(form.email, "Team Name: " + form.teamName, "Invite Code: " + teamCode, "Registration Info");
+  if (teamCode != null)
+      await sendEmail(form.email, "Team Name: " + form.teamName, "Invite Code: " + teamCode, "Registration Info");
   else await sendEmail(form.email, " ", " ", " ");
 
   response.status(200).json({
