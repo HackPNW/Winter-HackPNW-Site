c7248b22321799a5d78175342bcbc2b144c84d53
0xEquinox <WilliamJMcdonald@protonmail.com>
2023-01-04 20:33:07 -0800

    Ok fr this time it's good

--- api/register.js
+++ api/register.js
@@ -124,7 +124,7 @@ export default async function handler(request, response) {
     doc.teamId = null;
   }
 
-  if (form.teamName != undefined) await sendEmail(form.email, "Team Name: " + form.teamName, "Invite Code: " + teamCode, "Registration Info");
+  if (form.teamName != undefined && form.teamCode != undefined) await sendEmail(form.email, "Team Name: " + form.teamName, "Invite Code: " + teamCode, "Registration Info");
   else await sendEmail(form.email, " ", " ", " ");
 
   response.status(200).json({
