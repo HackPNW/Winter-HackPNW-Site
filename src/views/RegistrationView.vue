0bd6ecc1ebc5c35a135df18b3e2b4fd7b8a4f5a9
sexnine <25860388+sexnine@users.noreply.github.com>
2023-01-08 19:30:32 +1100

    Formatting

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -205,7 +205,11 @@
     <button
       class="transition mt-8 rounded-full bg-red-500 py-3 px-6 text-white font-bold text-xl w-fit"
       @click="register"
-      :class="{ 'bg-red-400': isSubmitting, 'bg-red-200': errors.length != 0, 'hover:bg-red-400': errors.length == 0 }">
+      :class="{
+        'bg-red-400': isSubmitting,
+        'bg-red-200': errors.length != 0,
+        'hover:bg-red-400': errors.length == 0,
+      }">
       <svg
         v-if="isSubmitting"
         class="animate-spin w-[76.73px] h-[27px] h-5 w-5 text-white"
@@ -366,7 +370,7 @@
 
   const invalidTeamCode = () => {
     alert("Something went wrong, please try again!");
-  }
+  };
 
   const register = () => {
     if (errors.value.length != 0) return;
